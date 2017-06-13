80235773d7f28d389ea530dc2a24f15e0f596109
fabiomonfardini <fabiomonfardini@192.168.0.101>
2017-06-12 23:58:57 -0300

    delete

--- UserPortal/src/main/java/com/bank/user/dao/RoleDao.java
+++
@@ -1,9 +0,0 @@
-package com.bank.user.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.bank.user.domains.security.Role;
-
-public interface RoleDao extends CrudRepository<Role, Long>{
-	Role findByName(String name);
-}
